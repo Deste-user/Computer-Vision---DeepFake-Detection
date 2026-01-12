946d711c94ad34ad4dcacbcb1b6dca94e4c36da1
Deste-user <marco.destefano1@edu.unifi.it>
2026-01-12 23:18:49 +0100

    Small Changes

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import ClipBased-SyntheticImageDetection/networks/openclipnet.py as openclipnet
 import torch
 import torch.nn as nn
-import open_clip
+#import open_clip
 import sys
 import os
 
+real_data_FFHQ_path = "/oblivion/Datasets/FFHQ/images1024x1024"
+fake_data_StyleGAN1_path = "/oblivion/Datasets/FFHQ/generated/ stylegan1-psi-0.5/images1024x1024"
+fake_data_StableDiffusion_path = " /oblivion/Datasets/FFHQ/generate/sdv1_4/images1024x1024"
 repo_path = os.path.join(os.getcwd(), 'ClipBased-SyntheticImageDetection')
 sys.path.append(repo_path)
 
 from networks import openclipnet
 
-levels = {0,5,10,15,20,23}
-model = openclipnet.OpenClipLinear()
+model = openclipnet.OpenClipLinear(layer_to_extract=23)
+
+
+
 
 
