cbb2b006fb44f0d4c4043c37a7384734be685b24
Deste-user <marco.destefano1@edu.unifi.it>
2026-02-16 20:33:29 +0100

    Modified the openclip class to take some extra embeddings of CLIP.

--- Report2/main_v2.py
+++ Report2/main_v2.py
@@ -21,7 +21,7 @@ fake_data_StyleGAN1_path = "/oblivion/Datasets/FFHQ/generated/stylegan1-psi-0.5/
 fake_data_StableDiffusion_path = "/oblivion/Datasets/FFHQ/generated/sdv1_4/images1024x1024"
 ACC_THRESHOLD = 0.5
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 repo_path = os.path.join(script_dir, 'ClipBased-SyntheticImageDetection')
 sys.path.append(repo_path)
 from networks import openclipnet
